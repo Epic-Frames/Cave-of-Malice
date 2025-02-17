6331a0f7b660d16884c035563a9ac7ff0870a456
Aenr <andreyermolin2@icloud.com>
2025-02-17 16:22:53 +0200

    Fixing Collision bug (world.py)

--- Contents/player.py
+++ Contents/player.py
@@ -44,21 +44,17 @@ class Player():
             self.xChange = -self.speed
         elif keys[pg.K_RIGHT]:
             self.xChange = self.speed
-        elif keys[pg.K_UP]:
-            self.yChange = self.speed
-        elif keys[pg.K_DOWN]:
-            self.yChange = -self.speed
         
-        # if keys[pg.K_UP] and self.on_ground:
-        #     self.jumping = True
-        #     self.yChange = self.jumpPower
-        #     self.on_ground = False
+        if keys[pg.K_UP] and self.on_ground:
+            self.jumping = True
+            self.yChange = self.jumpPower
+            self.on_ground = False
     
 
     def jump(self):
         # print(self.y, self.yChange)
         self.y -= self.yChange
-        # self.yChange -= self.gravity
+        self.yChange -= self.gravity
         # self.footY = self.y + self.height
         # print(self.y, self.yChange)
 
@@ -69,29 +65,18 @@ class Player():
 
         if pg.Rect.colliderect(playerXRect, tileList[1][1]):
             print("we did it again!")
-
-        # collideTest = pg.Rect.colliderect(playerXRect, pg.Rect(100, 100, 100, 100))
-        # if collideTest:
-        #     print("we did it")
-        # print('playerXRect', playerXRect)
-        # print('playerYRect', playerYRect)
         for tile in tileList:
             if pg.Rect.colliderect(playerXRect, tile[1]):
                 self.xChange = 0
             # print('tile[1]: ', tile[1])
             # print(self.rect.x, self.rect.y + self.yChange, self.width, self.height)
             if pg.Rect.colliderect(playerYRect, tile[1]):
-                print("OK")
                 if self.yChange < 0:
-                    print(tile[1])
-                    print(self.rect.x, self.rect.y - self.yChange, self.width, self.height)
                     self.y = tile[1].top - self.height
                     self.on_ground = True
                     self.yChange = 0
-                    print('yChange:', self.yChange)
                     return True
                 elif self.yChange > 0:
-                    print("NICE")
                     self.y = tile[1].bottom
                     self.yChange = 0
         return self.on_ground
